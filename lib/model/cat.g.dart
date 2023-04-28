6f389201f24e13f4f0bab4e2ae3ab7e746aa7426
DANIEL MERIB LISBOA <1125729@atitus.local>
2023-04-28 19:30:39 -0300

    a

---
+++ lib/model/cat.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cat.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Cat _$CatFromJson(Map<String, dynamic> json) => Cat(
+      id: json['id'] as String,
+      url: json['url'] as String,
+    );
+
+Map<String, dynamic> _$CatToJson(Cat instance) => <String, dynamic>{
+      'id': instance.id,
+      'url': instance.url,
+    };
